84d2d01be4f2438b231bbf45abfc05bcb1098816
Nicolas2-dev <nicolas.l.devoy@gmail.com>
2025-08-13 06:46:46 +0200

    update blocks

--- blocks/ephemerid.php
+++ blocks/ephemerid.php
@@ -3,21 +3,31 @@
 #autodoc ephemblock() : Bloc ephemerid <br />=> syntaxe : function#ephemblock
 function ephemblock()
 {
-   global $NPDS_Prefix, $gmt;
-   $cnt = 0;
-   $eday = date("d", time() + ((int)$gmt * 3600));
-   $emonth = date("m", time() + ((int)$gmt * 3600));
-   $result = sql_query("SELECT yid, content FROM " . $NPDS_Prefix . "ephem WHERE did='$eday' AND mid='$emonth' ORDER BY yid ASC");
-   $boxstuff = '<div>' . translate("En ce jour...") . '</div>';
-   while (list($yid, $content) = sql_fetch_row($result)) {
-      if ($cnt == 1)
-         $boxstuff .= "\n<br />\n";
-      $boxstuff .= "<b>$yid</b>\n<br />\n";
-      $boxstuff .= aff_langue($content);
-      $cnt = 1;
-   }
-   $boxstuff .= "<br />\n";
-   global $block_title;
-   $title = $block_title == '' ? translate("Ephémérides") : $block_title;
-   themesidebox($title, $boxstuff);
+    global $NPDS_Prefix, $gmt;
+
+    $cnt = 0;
+
+    $eday = date("d", time() + ((int)$gmt * 3600));
+    $emonth = date("m", time() + ((int)$gmt * 3600));
+
+    $result = sql_query("SELECT yid, content FROM " . $NPDS_Prefix . "ephem WHERE did='$eday' AND mid='$emonth' ORDER BY yid ASC");
+    $boxstuff = '<div>' . translate("En ce jour...") . '</div>';
+
+    while (list($yid, $content) = sql_fetch_row($result)) {
+        if ($cnt == 1) {
+            $boxstuff .= "\n<br />\n";
+        }
+
+        $boxstuff .= "<b>$yid</b>\n<br />\n";
+        $boxstuff .= aff_langue($content);
+
+        $cnt = 1;
+    }
+
+    $boxstuff .= "<br />\n";
+
+    global $block_title;
+    $title = $block_title == '' ? translate("Ephémérides") : $block_title;
+
+    themesidebox($title, $boxstuff);
 }
