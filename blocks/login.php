fa49d8694b51542e11badbac4fa036084c883243
Nicolas2-dev <nicolas.l.devoy@gmail.com>
2025-08-13 06:20:46 +0200

    update mainfile.php et creation du dossier block, update misqli

---
+++ blocks/login.php
@@ -0,0 +1,31 @@
+<?php
+
+#autodoc loginbox() : Bloc Login <br />=> syntaxe : function#loginbox
+function loginbox()
+{
+   global $user;
+   $boxstuff = '';
+   if (!$user) {
+      $boxstuff = '
+      <form action="user.php" method="post">
+         <div class="mb-3">
+            <label for="uname">' . translate("Identifiant") . '</label>
+            <input class="form-control" type="text" name="uname" maxlength="25" />
+         </div>
+         <div class="mb-3">
+            <label for="pass">' . translate("Mot de passe") . '</label>
+            <input class="form-control" type="password" name="pass" maxlength="20" />
+         </div>
+         <div class="mb-3">
+            <input type="hidden" name="op" value="login" />
+            <button class="btn btn-primary" type="submit">' . translate("Valider") . '</button>
+         </div>
+         <div class="help-block">
+         ' . translate("Vous n'avez pas encore de compte personnel ? Vous devriez") . ' <a href="user.php">' . translate("en créer un") . '</a>. ' . translate("Une fois enregistré") . ' ' . translate("vous aurez certains avantages, comme pouvoir modifier l'aspect du site,") . ' ' . translate("ou poster des commentaires signés...") . '
+         </div>
+      </form>';
+      global $block_title;
+      $title = $block_title == '' ? translate("Se connecter") : $block_title;
+      themesidebox($title, $boxstuff);
+   }
+}
---
+++ blocks/user.php
@@ -0,0 +1,14 @@
+<?php
+
+#autodoc userblock() : Bloc membre <br />=> syntaxe : function#userblock
+function userblock()
+{
+   global $NPDS_Prefix, $user, $cookie;
+   if (($user) and ($cookie[8])) {
+      $getblock = Q_select("SELECT ublock FROM " . $NPDS_Prefix . "users WHERE uid='$cookie[0]'", 86400);
+      $ublock = $getblock[0];
+      global $block_title;
+      $title = $block_title == '' ? translate("Menu de") . ' ' . $cookie[1] : $block_title;
+      themesidebox($title, $ublock['ublock']);
+   }
+}
