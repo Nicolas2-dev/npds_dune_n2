a70460f4869d6cbcddb5576ddf6fabc38428010e
Nicolas2-dev <nicolas.l.devoy@gmail.com>
2025-08-13 19:21:05 +0200

    refactoring fichiers

--- top.php
+++ top.php
@@ -10,37 +10,43 @@
 /* it under the terms of the GNU General Public License as published by */
 /* the Free Software Foundation; either version 3 of the License.       */
 /************************************************************************/
-if (!function_exists("Mysql_Connexion"))
-    include("mainfile.php");
 
-if ($SuperCache)
-    $cache_obj = new cacheManager();
-else
-    $cache_obj = new SuperCacheEmpty();
+if (!function_exists('Mysql_Connexion')) {
+    include 'mainfile.php';
+}
 
-include("header.php");
+include 'header.php';
 
-if (($SuperCache) and (!$user))
+$cache_obj = $SuperCache ? new cacheManager() : new SuperCacheEmpty();
+
+if (($SuperCache) and (!$user)) {
     $cache_obj->startCachingPage();
+}
 
 if (($cache_obj->genereting_output == 1) or ($cache_obj->genereting_output == -1) or (!$SuperCache) or ($user)) {
     $inclusion = false;
-    if (file_exists("themes/$theme/html/top.html"))
-        $inclusion = "themes/$theme/html/top.html";
-    elseif (file_exists("themes/default/html/top.html"))
-        $inclusion = "themes/default/html/top.html";
-    else
-        echo "html/top.html / not find !<br />";
+    if (file_exists($path = 'themes/' . $theme . '/html/top.html')) {
+        $inclusion = $path;
+
+    } elseif (file_exists($path = 'themes/default/html/top.html')) {
+        $inclusion = $path;
+    } else {
+        echo 'html/top.html / not find !<br />';
+    }
+
     if ($inclusion) {
         ob_start();
-        include($inclusion);
-        $Xcontent = ob_get_contents();
+            include $inclusion;
+            $Xcontent = ob_get_contents();
         ob_end_clean();
+
         echo meta_lang(aff_langue($Xcontent));
     }
 }
 
 // -- SuperCache
-if (($SuperCache) and (!$user))
+if (($SuperCache) and (!$user)) {
     $cache_obj->endCachingPage();
-include("footer.php");
+}
+
+include 'footer.php';
