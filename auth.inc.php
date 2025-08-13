a7c0bde37a6d7f0fbb6f10936655efa2d9f8d610
Nicolas2-dev <nicolas.l.devoy@gmail.com>
2025-08-13 23:17:10 +0200

    refactoring fichiers

--- auth.inc.php
+++ auth.inc.php
@@ -16,61 +16,92 @@
 function Admin_alert($motif)
 {
     global $admin;
+
     setcookie('admin', '', 0);
     unset($admin);
 
     Ecr_Log('security', 'auth.inc.php/Admin_alert : ' . $motif, '');
-    $Titlesitename = 'NPDS';
-    if (file_exists("meta/meta.php"))
-        include("meta/meta.php");
+
+    import_meta();
+
     echo '
-      </head>
-      <body>
-         <br /><br /><br />
-         <p style="font-size: 24px; font-family: Tahoma, Arial; color: red; text-align:center;"><strong>.: ' . translate("Votre adresse Ip est enregistrée") . ' :.</strong></p>
-      </body>
-   </html>';
+        </head>
+        <body>
+            <br /><br /><br />
+            <p style="font-size: 24px; font-family: Tahoma, Arial; color: red; text-align:center;">
+                <strong>.: ' . translate('Votre adresse Ip est enregistrée') . ' :.</strong>
+            </p>
+        </body>
+    </html>';
     die();
 }
 
 if ((isset($aid)) and (isset($pwd)) and ($op == 'login')) {
     if ($aid != '' and $pwd != '') {
-        $result = sql_query("SELECT pwd, hashkey FROM " . $NPDS_Prefix . "authors WHERE aid='$aid'");
+
+        $result = sql_query("SELECT pwd, hashkey 
+                             FROM " . sql_prefix('authors') . " 
+                             WHERE aid='$aid'");
+
         if (sql_num_rows($result) == 1) {
+
             $setinfo = sql_fetch_assoc($result);
+
             $dbpass = $setinfo['pwd'];
-            $pwd = (PHP_VERSION_ID >= 80200) ?
-                mb_convert_encoding($pwd, 'ISO-8859-1', 'UTF-8') :
-                utf8_decode($pwd);
+
+            $pwd = (PHP_VERSION_ID >= 80200)
+                ? mb_convert_encoding($pwd, 'ISO-8859-1', 'UTF-8')
+                : utf8_decode($pwd);
+
             $scryptPass = null;
 
             if (password_verify($pwd, $dbpass) or (strcmp($dbpass, $pwd) == 0)) {
                 if (!$setinfo['hashkey']) {
-                    $AlgoCrypt = PASSWORD_BCRYPT;
-                    $min_ms = 100;
-                    $options = ['cost' => getOptimalBcryptCostParameter($pwd, $AlgoCrypt, $min_ms)];
-                    $hashpass = password_hash($pwd, $AlgoCrypt, $options);
+
+                    $AlgoCrypt  = PASSWORD_BCRYPT;
+                    $min_ms     = 100;
+
+                    $options    = [
+                        'cost' => getOptimalBcryptCostParameter($pwd, $AlgoCrypt, $min_ms)
+                    ];
+                    
+                    $hashpass   = password_hash($pwd, $AlgoCrypt, $options);
+
                     $pwd = crypt($pwd, $hashpass);
-                    sql_query("UPDATE " . $NPDS_Prefix . "authors SET pwd='$pwd', hashkey='1' WHERE aid='$aid'");
-                    $result = sql_query("SELECT pwd, hashkey FROM " . $NPDS_Prefix . "authors WHERE aid = '$aid'");
-                    if (sql_num_rows($result) == 1)
+
+                    sql_query("UPDATE " . sql_prefix('authors') . " 
+                               SET pwd='$pwd', hashkey='1' 
+                               WHERE aid='$aid'");
+
+                    $result = sql_query("SELECT pwd, hashkey 
+                                         FROM " . sql_prefix('authors') . " 
+                                         WHERE aid = '$aid'");
+
+                    if (sql_num_rows($result) == 1) {
                         $setinfo = sql_fetch_assoc($result);
+                    }
+
                     $dbpass = $setinfo['pwd'];
                     $scryptPass = crypt($dbpass, $hashpass);
                 }
             }
 
-            if (password_verify($pwd, $dbpass))
+            if (password_verify($pwd, $dbpass)) {
                 $CryptpPWD = $dbpass;
-            elseif (password_verify($dbpass, $scryptPass) or strcmp($dbpass, $pwd) == 0)
+            } elseif (password_verify($dbpass, $scryptPass) or strcmp($dbpass, $pwd) == 0) {
                 $CryptpPWD = $pwd;
-            else
-                Admin_Alert("Passwd not in DB#1 : $aid");
+            } else {
+                Admin_Alert(sprintf('Passwd not in DB#1 : ', $aid));
+            }
 
             $admin = base64_encode("$aid:" . md5($CryptpPWD));
-            if ($admin_cook_duration <= 0)
+
+            if ($admin_cook_duration <= 0) {
                 $admin_cook_duration = 1;
+            }
+
             $timeX = time() + (3600 * $admin_cook_duration);
+
             setcookie('admin', $admin, $timeX);
             setcookie('adm_exp', $timeX, $timeX);
         }
@@ -84,21 +115,32 @@ $super_admintest = false;
 if (isset($admin) and ($admin != '')) {
     $Xadmin = base64_decode($admin);
     $Xadmin = explode(':', $Xadmin);
+
     $aid = urlencode($Xadmin[0]);
+
     $AIpwd = $Xadmin[1];
-    if ($aid == '' or $AIpwd == '')
+
+    if ($aid == '' or $AIpwd == '') {
         Admin_Alert('Null Aid or Passwd');
-    $result = sql_query("SELECT pwd, radminsuper FROM " . $NPDS_Prefix . "authors WHERE aid = '$aid'");
-    if (!$result)
-        Admin_Alert("DB not ready #2 : $aid / $AIpwd");
-    else {
+    }
+
+    $result = sql_query("SELECT pwd, radminsuper 
+                         FROM " . sql_prefix('authors') . " 
+                         WHERE aid = '$aid'");
+
+    if (!$result) {
+        Admin_Alert(sprintf('DB not ready #2 : %s / %s', $aid, $AIpwd));
+    } else {
         list($AIpass, $Xsuper_admintest) = sql_fetch_row($result);
+
         if (md5($AIpass) == $AIpwd and $AIpass != '') {
             $admintest = true;
             $super_admintest = $Xsuper_admintest;
-        } else
-            Admin_Alert("Password in Cookies not Good #1 : $aid / $AIpwd");
+        } else {
+            Admin_Alert(printf('Password in Cookies not Good #1 : %s / %s', $aid, $AIpwd));
+        }
     }
+
     unset($AIpass);
     unset($AIpwd);
     unset($Xadmin);
--- chat.php
+++ chat.php
@@ -14,20 +14,25 @@
 // gp_id=ID du groupe au sens NPDS du terme => 0 : tous / -127 : Admin / -1 : Anonyme / 1 : membre / 2 ... 126 : groupe de membre
 // token_de_securite = encrypt(serialize(gp_id)) => Permet d'éviter le lancement du Chat sans autorisation
 
-//if (!defined('NPDS_GRAB_GLOBALS_INCLUDED'))
-//   include("grab_globals.php");
+if (!function_exists('Mysql_Connexion')) {
+    include 'mainfile.php';
+}
 
-if (!function_exists("Mysql_Connexion"))
-    include("mainfile.php");
-
-$Titlesitename = 'NPDS';
 $meta_op = '';
 $meta_doctype = '<!DOCTYPE html>';
-include("meta/meta.php");
+
+import_meta('NPDS');
+
 echo '
-         <link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon" />
-      </head>  
-         <div style="height:1vh;" class=""><iframe src="chatrafraich.php?repere=0&amp;aff_entetes=1&amp;connectes=-1&amp;id=' . $id . '&amp;auto=' . $auto . '" frameborder="0" scrolling="no" noresize="noresize" name="rafraich" width="100%" height="100%"></iframe></div>
-         <div style="height:58vh;" class=""><iframe src="chattop.php" frameborder="0" scrolling="yes" noresize="noresize" name="haut" width="100%" height="100%"></iframe></div>
-         <div style="height:39vh;" class=""><iframe src="chatinput.php?id=' . $id . '&amp;auto=' . $auto . '" frameborder="0" scrolling="yes" noresize="noresize" name="bas" width="100%" height="100%"></iframe></div>
-   </html>';
+    <link rel="shortcut icon" href="assets/images/favicon.ico" type="image/x-icon" />
+    </head>  
+    <div style="height:1vh;" class="">
+        <iframe src="chatrafraich.php?repere=0&amp;aff_entetes=1&amp;connectes=-1&amp;id=' . $id . '&amp;auto=' . $auto . '" frameborder="0" scrolling="no" noresize="noresize" name="rafraich" width="100%" height="100%"></iframe>
+    </div>
+    <div style="height:58vh;" class="">
+        <iframe src="chattop.php" frameborder="0" scrolling="yes" noresize="noresize" name="haut" width="100%" height="100%"></iframe>
+    </div>
+    <div style="height:39vh;" class="">
+        <iframe src="chatinput.php?id=' . $id . '&amp;auto=' . $auto . '" frameborder="0" scrolling="yes" noresize="noresize" name="bas" width="100%" height="100%"></iframe>
+    </div>
+</html>';
