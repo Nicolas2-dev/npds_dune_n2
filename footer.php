6478762b077d99e06128f8e533a09e33a47b9d83
Nicolas2-dev <nicolas.l.devoy@gmail.com>
2025-08-13 03:59:12 +0200

    update footer.php

--- footer.php
+++ footer.php
@@ -12,58 +12,97 @@
 /* it under the terms of the GNU General Public License as published by */
 /* the Free Software Foundation; either version 3 of the License.       */
 /************************************************************************/
-if (!function_exists("Mysql_Connexion"))
-   include("mainfile.php");
+
+if (!function_exists('Mysql_Connexion')) {
+   include 'mainfile.php';
+}
 
 function footmsg()
 {
    global $foot1, $foot2, $foot3, $foot4;
+
    $foot = '<p align="center">';
-   if ($foot1) $foot .= stripslashes($foot1) . '<br />';
-   if ($foot2) $foot .= stripslashes($foot2) . '<br />';
-   if ($foot3) $foot .= stripslashes($foot3) . '<br />';
-   if ($foot4) $foot .= stripslashes($foot4);
+
+   // Boucle sur les variables $foot1 à $foot4
+   for ($i = 1; $i <= 4; $i++) {
+      $varName = 'foot' . $i;
+      if (!empty($$varName)) {
+         $foot .= stripslashes($$varName);
+         if ($i < 4) {
+            $foot .= '<br />';
+         }
+      }
+   }
+
    $foot .= '</p>';
+
    echo aff_langue($foot);
 }
 
 function foot()
 {
    global $user, $Default_Theme, $cookie9;
+
    if ($user) {
-      $user2 = base64_decode($user);
-      $cookie = explode(':', $user2);
-      if ($cookie[9] == '') $cookie[9] = $Default_Theme;
+      $cookie = explode(':', base64_decode($user));
+
+      if ($cookie[9] == '') {
+         $cookie[9] = $Default_Theme;
+      }
 
       $ibix = explode('+', urldecode($cookie[9]));
-      if (!$file = @opendir("themes/$ibix[0]"))
-         include("themes/$Default_Theme/footer.php");
-      else
-         include("themes/$ibix[0]/footer.php");
-   } else
-      include("themes/$Default_Theme/footer.php");
-   if ($user) $cookie9 = $ibix[0];
+
+      if (!@opendir('themes/'.$ibix[0])) {
+         $theme = $Default_Theme;
+      } else {
+         $theme = $ibix[0];
+      }
+   } else {
+      $theme = $Default_Theme;
+   }
+
+   include 'themes/' . $theme . '/footer.php';
+
+   if ($user) {
+      $cookie9 = $ibix[0];
+   }
 }
 
-global $tiny_mce, $cookie9, $Default_Theme;
-if ($tiny_mce)
+function footer_after($theme) {
+   if (file_exists($path_theme = 'themes/' . $theme . '/include/footer_after.inc')) {
+      include $path_theme;
+   } else {
+      if (file_exists($path_module = 'modules/include/footer_after.inc')) {
+         include $path_module;
+      }
+   }
+}
+
+global $tiny_mce;
+if ($tiny_mce) {
    echo aff_editeur('tiny_mce', 'end');
+}
+
 // include externe file from modules/include for functions, codes ...
-if (file_exists("modules/include/footer_before.inc")) include("modules/include/footer_before.inc");
+if (file_exists($path_module = 'modules/include/footer_before.inc')) {
+   include $path_module;
+}
+
 foot();
+
 // include externe file from modules/themes include for functions, codes ...
 if (isset($user)) {
-   if (file_exists("themes/$cookie9/include/footer_after.inc")) include("themes/$cookie9/include/footer_after.inc");
-   else
-         if (file_exists("modules/include/footer_after.inc")) include("modules/include/footer_after.inc");
+   global $cookie9;
+   footer_after($cookie9);
 } else {
-   if (file_exists("themes/$Default_Theme/include/footer_after.inc")) include("themes/$Default_Theme/include/footer_after.inc");
-   else
-         if (file_exists("modules/include/footer_after.inc")) include("modules/include/footer_after.inc");
+   global $Default_Theme;
+   footer_after($Default_Theme);
 }
+
 echo '
    </body>
 </html>';
 
-include("sitemap.php");
+include 'sitemap.php';
+
 sql_close();
