14294c4cf1d4d7f60fedec311d3561c265e7153e
Nicolas2-dev <nicolas.l.devoy@gmail.com>
2025-08-14 00:53:07 +0200

    refactoring fichiers

--- faq.php
+++ faq.php
@@ -12,86 +12,109 @@
 /* it under the terms of the GNU General Public License as published by */
 /* the Free Software Foundation; either version 3 of the License.       */
 /************************************************************************/
-if (!function_exists("Mysql_Connexion"))
-    include("mainfile.php");
+
+if (!function_exists('Mysql_Connexion')) {
+    include 'mainfile.php';
+}
 
 function ShowFaq($id_cat, $categories)
 {
-    global $sitename, $NPDS_Prefix;
     echo '
-   <h2 class="mb-4">' . translate("FAQ - Questions fréquentes") . '</h2>
-   <hr />
-   <h3 class="mb-3">' . translate("Catégorie") . ' <span class="text-body-secondary"># ' . StripSlashes($categories) . '</span></h3>
-   <p class="lead">
-      <a href="faq.php" title="' . translate("Retour à l'index FAQ") . '" data-bs-toggle="tooltip">Index</a>&nbsp;&raquo;&raquo;&nbsp;' . StripSlashes($categories) . '
-   </p>';
+    <h2 class="mb-4">' . translate("FAQ - Questions fréquentes") . '</h2>
+    <hr />
+    <h3 class="mb-3">' . translate("Catégorie") . ' <span class="text-body-secondary"># ' . StripSlashes($categories) . '</span></h3>
+    <p class="lead">
+        <a href="faq.php" title="' . translate("Retour à l'index FAQ") . '" data-bs-toggle="tooltip">Index</a>&nbsp;&raquo;&raquo;&nbsp;' . StripSlashes($categories) . '
+    </p>';
 
-    $result = sql_query("SELECT id, id_cat, question, answer FROM " . $NPDS_Prefix . "faqanswer WHERE id_cat='$id_cat'");
+    $result = sql_query("SELECT id, id_cat, question, answer 
+                         FROM " . sql_prefix('faqanswer') . " 
+                         WHERE id_cat='$id_cat'");
+
     while (list($id, $id_cat, $question, $answer) = sql_fetch_row($result)) {
     }
 }
 
 function ShowFaqAll($id_cat)
 {
-    global $NPDS_Prefix;
-    $result = sql_query("SELECT id, id_cat, question, answer FROM " . $NPDS_Prefix . "faqanswer WHERE id_cat='$id_cat'");
+    $result = sql_query("SELECT id, id_cat, question, answer 
+                         FROM " . sql_prefix('faqanswer') . " 
+                         WHERE id_cat='$id_cat'");
+
     while (list($id, $id_cat, $question, $answer) = sql_fetch_row($result)) {
-        echo '
-      <div class="card mb-3" id="accordion_' . $id . '" role="tablist" aria-multiselectable="true">
-         <div class="card-body">
-            <h4 class="card-title">
-            <a data-bs-toggle="collapse" data-parent="#accordion_' . $id . '" href="#faq_' . $id . '" aria-expanded="true" aria-controls="' . $id . '"><i class="fa fa-caret-down toggle-icon"></i></a>&nbsp;' . aff_langue($question) . '
-            </h4>
-            <div class="collapse" id="faq_' . $id . '" >
-               <div class="card-text">
-               ' . meta_lang(aff_langue($answer)) . '
-               </div>
+        echo '<div class="card mb-3" id="accordion_' . $id . '" role="tablist" aria-multiselectable="true">
+            <div class="card-body">
+                <h4 class="card-title">
+                <a data-bs-toggle="collapse" data-parent="#accordion_' . $id . '" href="#faq_' . $id . '" aria-expanded="true" aria-controls="' . $id . '"><i class="fa fa-caret-down toggle-icon"></i></a>&nbsp;' . aff_langue($question) . '
+                </h4>
+                <div class="collapse" id="faq_' . $id . '" >
+                <div class="card-text">
+                ' . meta_lang(aff_langue($answer)) . '
+                </div>
+                </div>
             </div>
-         </div>
-      </div>';
+        </div>';
     }
 }
 
 settype($myfaq, 'string');
+
 if (!$myfaq) {
-    include("header.php");
+
+    include 'header.php';
+
     // Include cache manager
     if ($SuperCache) {
         $cache_obj = new cacheManager();
         $cache_obj->startCachingPage();
-    } else
+    } else {
         $cache_obj = new SuperCacheEmpty();
+    }
+
     if (($cache_obj->genereting_output == 1) or ($cache_obj->genereting_output == -1) or (!$SuperCache)) {
-        $result = sql_query("SELECT id_cat, categories FROM " . $NPDS_Prefix . "faqcategories ORDER BY id_cat ASC");
-        echo '
-       <h2 class="mb-4">' . translate("FAQ - Questions fréquentes") . '</h2>
-       <hr />
-       <h3 class="mb-3">' . translate("Catégories") . '<span class="badge bg-secondary float-end">' . sql_num_rows($result) . '</span></h3>
-       <div class="list-group">';
+        $result = sql_query("SELECT id_cat, categories 
+                             FROM " . sql_prefix('faqcategories') . " 
+                             ORDER BY id_cat ASC");
+
+        echo '<h2 class="mb-4">' . translate("FAQ - Questions fréquentes") . '</h2>
+        <hr />
+        <h3 class="mb-3">' . translate("Catégories") . '<span class="badge bg-secondary float-end">' . sql_num_rows($result) . '</span></h3>
+        <div class="list-group">';
+
         while (list($id_cat, $categories) = sql_fetch_row($result)) {
             $catname = urlencode(aff_langue($categories));
             echo '<a class="list-group-item list-group-item-action" href="faq.php?id_cat=' . $id_cat . '&amp;myfaq=yes&amp;categories=' . $catname . '"><h4 class="list-group-item-heading">' . aff_langue($categories) . '</h4></a>';
         }
-        echo '
-       </div>';
+
+        echo '</div>';
     }
-    if ($SuperCache)
+
+    if ($SuperCache) {
         $cache_obj->endCachingPage();
-    include("footer.php");
+    }
+
+    include 'footer.php';
 } else {
     $title = "FAQ : " . removeHack(StripSlashes($categories));
-    include("header.php");
+
+    include 'header.php';
+
     // Include cache manager
     if ($SuperCache) {
         $cache_obj = new cacheManager();
         $cache_obj->startCachingPage();
-    } else
+    } else {
         $cache_obj = new SuperCacheEmpty();
+    }
+
     if (($cache_obj->genereting_output == 1) or ($cache_obj->genereting_output == -1) or (!$SuperCache)) {
         ShowFaq($id_cat, removeHack($categories));
         ShowFaqAll($id_cat);
     }
-    if ($SuperCache)
+
+    if ($SuperCache) {
         $cache_obj->endCachingPage();
-    include("footer.php");
+    }
+
+    include 'footer.php';
 }
