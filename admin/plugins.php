30fe72ffae2844b8f41c04bd687ce86ed19c98fe
Nicolas2-dev <nicolas.l.devoy@gmail.com>
2025-08-15 00:49:23 +0200

    refactoring fichiers

--- admin/plugins.php
+++ admin/plugins.php
@@ -13,12 +13,16 @@
 /* the Free Software Foundation; either version 3 of the License.       */
 /************************************************************************/
 
-if (!function_exists('admindroits'))
-   include('die.php');
+if (!function_exists('admindroits')) {
+   include 'die.php';
+}
 
 include 'header.php';
+
 if ($ModPath != '') {
-   if (file_exists("modules/$ModPath/$ModStart.php"))
-      include("modules/$ModPath/$ModStart.php");
-} else
+   if (file_exists('modules/'.$ModPath.'/'.$ModStart.'.php')) {
+      include 'modules/'.$ModPath.'/'.$ModStart.'.php';
+   }
+} else {
    redirect_url(urldecode($ModStart));
+}
