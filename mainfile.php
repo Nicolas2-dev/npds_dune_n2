751e0a60cfd6da3885f530705a7208b7be0bb894
Nicolas2-dev <nicolas.l.devoy@gmail.com>
2025-08-17 05:26:28 +0200

    refactoring fichiers

--- mainfile.php
+++ mainfile.php
@@ -3661,3 +3661,21 @@ function import_meta($title = 'NPDS')
         include 'storage/meta/meta.php';
     }
 }
+
+function theme_list()
+{
+    $handle = opendir('themes');
+
+    while (false !== ($file = readdir($handle))) {
+        if (($file[0] !== '_') 
+        and (!strstr($file, '.')) 
+        and (!strstr($file, '__vierge')) 
+        and (!strstr($file, 'base')))
+            $themelist[] = $file;
+    }
+
+    natcasesort($themelist);
+    closedir($handle); 
+
+    return implode(' ', $themelist);
+}
