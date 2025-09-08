37067c856b4863f97f500a90a4ffde71ea46f234
Nicolas2-dev <nicolas.l.devoy@gmail.com>
2025-09-08 09:16:10 +0200

    add helpers block functions

---
+++ app/Library/Block/helpers.php
@@ -0,0 +1,39 @@
+<?php
+
+use App\Library\Block\Block;
+
+// Block Functions.
+
+if (! function_exists('leftBlocks')) {
+    /**
+     * Exécute l'affichage ou le traitement des blocs positionnés à gauche.
+     *
+     * Cette fonction est un helper pour gérer les blocs de la colonne gauche
+     * avec des classes CSS supplémentaires si nécessaire.
+     *
+     * @param  string  $moreclass Classes CSS supplémentaires à appliquer aux blocs.
+     *
+     * @return void
+     */
+    function leftBlocks(string $moreclass): void
+    {
+        Block::leftBlocks($moreclass);
+    }
+}
+
+if (! function_exists('rightBlocks')) {
+    /**
+     * Exécute l'affichage ou le traitement des blocs positionnés à droite.
+     *
+     * Cette fonction est un helper pour gérer les blocs de la colonne droite
+     * avec des classes CSS supplémentaires si nécessaire.
+     *
+     * @param  string  $moreclass Classes CSS supplémentaires à appliquer aux blocs.
+     *
+     * @return void
+     */
+    function rightBlocks(string $moreclass): void
+    {
+        Block::rightBlocks($moreclass);
+    }
+}
